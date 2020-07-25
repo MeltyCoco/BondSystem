0abae996b8e48de7f87a30f7cfccf9902219316c
MeltyCoco <37313092+MeltyCoco@users.noreply.github.com>
2020-07-24 21:36:00 -0700

    Add files via upload

--- gacha/__init__.py
+++ gacha/__init__.py
@@ -1,8 +1,5 @@
 from .bondsystem import Bondsystem
-from redbot.core import data_manager
 
 
 async def setup(bot):
-    bond = Bondsystem(bot)
-    data_manager.bundled_data_path(bond)
-    bot.add_cog(bond)
+    bot.add_cog(Bondsystem(bot))
