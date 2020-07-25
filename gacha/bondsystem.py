ab07502cf1d405d0599fdec1fb5cf70288ecc25f
MeltyCoco <37313092+MeltyCoco@users.noreply.github.com>
2020-07-24 21:16:41 -0700

    Update bondsystem.py

--- gacha/bondsystem.py
+++ gacha/bondsystem.py
@@ -40,18 +40,6 @@ class Bondsystem(Cog):
             wishlist={},
         )
 
-    def __str__(self):
-        return (
-            "Name {}\n"
-            "Inventory: {}\n"
-            "Wishlist: {}".format(self.user, self.inventory, self.wishlist)
-        )
-
-    def __repr__(self):
-        return "{} - {} - {}".format(
-            self.user, self.inventory, self.wishlist,
-        )
-
     async def _send_message(channel, message):
         """Sends a message"""
 
